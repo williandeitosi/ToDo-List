736500dad124454bf52ce7ab24e9051a3fb0ca96
willian <williandeitosi@gmail.com>
2022-11-06 11:54:14 -0300

    create check box, and style list

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./componentes/Form/form";
 import List from "./componentes/List/List";
-import Item from "./Item/Item";
-
+import Item from "./componentes/Item/Item";
 
 function App() {
   const [items, setItems] = useState([]);
 
   function onAddItem(text) {
-
-    let it  = new Item(text)
+    let it = new Item(text);
 
     setItems([...items, it]);
   }
 
-  function onItemDeleted (item) {
+  function onItemDeleted(item) {
+    let filteredItems = items.filter((it) => it.id != item.id);
+    setItems(filteredItems);
+  }
 
-    let filteredItems = items.filter(it => it.id != item.id)
-    setItems(filteredItems)
+  function onDone(item) {
+    let updatedItems = items.map((it) => {
+      if (it.id === item.id) {
+        it.done = !it.done;
+      }
+      return it;
+    });
 
+    setItems(updatedItems);
   }
 
   return (
@@ -28,7 +35,7 @@ function App() {
 
       <Form onAddItem={onAddItem}></Form>
 
-      <List onItemDeleted={onItemDeleted} items={items}></List>
+      <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
     </div>
   );
 }
--- src/componentes/List/List.jsx
+++ src/componentes/List/List.jsx
@@ -1,26 +1,39 @@
 import trash from "../../assets/caixa.png";
+import check from "../../assets/check.png";
+import square from "../../assets/square.png"
 import "./List.css";
 
-function List(props) {
+function DoneImg(props) {
 
-
-
-  function deleteItem (item) {
-    console.log(item)
+  if (props.done) {
+    return (<img src={check} alt="check" width={20} />)
+  } else {
+    return (<img src={square} alt="square" width={20} />)
   }
 
+}
 
- 
+function List(props) {
   return (
     <ul>
-      {props.items.map(item => 
-        <li key={item.id}>{item.text}
+      {props.items.map((item) => (
+        <li className={item.done?"done item":"item"} key={item.id}>
+          {item.text}
 
-          <button onClick={() => {props.onItemDeleted(item)}}>
-            <img src={trash} alt="trash" width={20} />
-          </button>
+          <div>
+            <button onClick={() => {props.onDone(item)}}>
+              <DoneImg done={item.done}/>
+            </button>
+            <button
+              onClick={() => {
+                props.onItemDeleted(item);
+              }}
+            >
+              <img src={trash} alt="trash" width={20} />
+            </button>
+          </div>
         </li>
-      )}
+      ))}
     </ul>
   );
 }
